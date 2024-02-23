a6d0a5e61b495ec555bb131024948d633adc3c07
YoungWoo Kim <70052797+kbc1235@users.noreply.github.com>
2024-02-23 15:38:16 +0900

    환경 변수 설정

--- src/components/atoms/Maps/Maps.tsx
+++ src/components/atoms/Maps/Maps.tsx
@@ -1,21 +1,27 @@
-import { Map } from "react-kakao-maps-sdk";
-import useKakaoLoader from "../../../hook/useKakaoLoader";
+import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
 export default function Maps() {
-  useKakaoLoader();
+  const [loading, error] = useKakaoLoader({
+    appkey: process.env.VITE_APP_KAKAO_API_KEY || "",
+    libraries: ["services"],
+  });
   return (
-    <Map // 지도를 표시할 Container
-      id="map"
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "350px",
-      }}
-      level={3} // 지도의 확대 레벨
-    />
+    <>
+      {loading && <div>loading...</div>}
+      {error && <div>error...</div>}
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          // 지도의 크기
+          width: "100%",
+          height: "350px",
+        }}
+        level={3} // 지도의 확대 레벨
+      />
+    </>
   );
 }
