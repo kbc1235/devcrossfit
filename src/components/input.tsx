ee57b7f911dc6ebfbbc12ad9b6d320de97724df0
김영우 <ywkim0114@riskzero.kr>
2024-04-08 11:35:06 +0900

    onClick 추가

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -19,6 +19,7 @@ export default function Input({
     <InputCustom
       type={type}
       onChange={onChange}
+      onClick={onClick}
       value={value}
       placeholder={placeholder}
       readOnly={readOnly}
