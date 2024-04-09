8b3af2dd224595761d0880c419e4d6f1f774b80a
김영우 <ywkim0114@riskzero.kr>
2024-04-09 09:26:55 +0900

    zindex 수정

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -108,6 +108,7 @@ const MarkerInfo = styled.div`
   padding: 10px;
   border-radius: 5px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  z-index: 1001;
 `;
 
 const CustomMarkerWrapper = styled.div`
